58cb309013500c594a8de08705a670683c7525dd
ArthurLSouza <150375423+ArthurLSouza@users.noreply.github.com>
2024-07-15 17:36:45 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,18 +1,15 @@
 $(document).ready(function() {
     $('#formTarefa').submit(function(event) {
-        event.preventDefault(); // Evita o comportamento padrão do formulário
-
+        event.preventDefault(); 
         var novaTarefa = $('#tarefaInput').val().trim();
 
         if (novaTarefa !== '') {
             var li = $('<li></li>').text(novaTarefa);
             $('#listaTarefas').append(li);
-            $('#tarefaInput').val(''); // Limpa o campo de entrada
+            $('#tarefaInput').val(''); 
         }
     });
-
-    // Função para marcar uma tarefa como concluída
-    $('#listaTarefas').on('click', 'li', function() {
+     $('#listaTarefas').on('click', 'li', function() {
         $(this).toggleClass('completed');
     });
 });
